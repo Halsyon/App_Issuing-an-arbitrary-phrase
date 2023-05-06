e641bdf6ca2b72e1ecfe4e96a2bf61083bf9b473
GrandEmetak <JekHamond@tutanota.com>
2023-05-06 14:33:06 +0500

    fix: updating model app info
    persistence

--- src/main/java/com/customer/botphrase/model/Bot.java
+++ src/main/java/com/customer/botphrase/model/Bot.java
@@ -1,6 +1,24 @@
+/*
+Copyright (c) 2016-2023 VMware Inc. or its affiliates, All Rights Reserved.[Halsyon]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
 package com.customer.botphrase.model;
 
 import com.customer.botphrase.repository.Storage;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -17,16 +35,15 @@ import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 
 @Component
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class Bot extends TelegramLongPollingBot {
-    private static final Logger LOGGER = LoggerFactory.getLogger(Bot.class);
-    private static final String BOT_TOKEN = "****"; //токен выдан телеграм
-    private static final String BOT_NAME = "****"; //имя бота
 
-    private final Storage storage;
+    static Logger LOGGER = LoggerFactory.getLogger(Bot.class);
+    static String BOT_TOKEN = "****"; //токен выдан телеграм
+    static String BOT_NAME = "****"; //имя бота
 
-    public Bot(Storage storage) {
-        this.storage = storage;
-    }
+    Storage storage;
 
     @Override
     public String getBotUsername() {
@@ -44,16 +61,11 @@ public class Bot extends TelegramLongPollingBot {
             if (update.hasMessage() && update.getMessage().hasText()) {
 
                 Message inMess = update.getMessage();
-
                 String chatId = inMess.getChatId().toString();
-
                 String response = parseMessage(inMess.getText());
-
                 SendMessage outMess = new SendMessage();
-
                 outMess.setChatId(chatId);
                 outMess.setText(response);
-
                 execute(outMess);
             }
         } catch (TelegramApiException e) {
@@ -65,7 +77,7 @@ public class Bot extends TelegramLongPollingBot {
         String response;
         if (textMsg.equals("/start")) {
             response = "Приветствую, бот знает много цитат. Жми /get, чтобы получить случайную из них";
-        } else if (textMsg.equals("/get") || textMsg.equals("Просвяти")) { //todo
+        } else if (textMsg.equals("/get") || textMsg.equals("Про святи")) {
             response = storage.getRandQuote();
         } else {
             response = "Сообщение не распознано";
@@ -84,7 +96,7 @@ public class Bot extends TelegramLongPollingBot {
         KeyboardRow keyboardRow = new KeyboardRow();
         keyboardRows.add(keyboardRow);
 
-        keyboardRow.add(new KeyboardButton("Просвяти"));
+        keyboardRow.add(new KeyboardButton("Про святи"));
 
         replyKeyboardMarkup.setKeyboard(keyboardRows);
     }
--- src/main/java/com/customer/botphrase/model/Expression.java
+++ src/main/java/com/customer/botphrase/model/Expression.java
@@ -1,22 +1,38 @@
+/*
+Copyright (c) 2016-2023 VMware Inc. or its affiliates, All Rights Reserved.[Halsyon]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
 package com.customer.botphrase.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import javax.persistence.*;
 
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-
-@Data
+/**
+ * @description main model
+ */
+@Getter
+@Setter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
-//@Entity
+@Entity(name = "Expression")
 public class Expression {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id; // todo Long before DB insert
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "expression_id")
+    private Long id;
     private String citation;
     private String author;
 }
